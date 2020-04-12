51d5d9bfdbaf6c471ae8009622ccda77f55228ba
TheDarkLad <jean-paul.kommers@outlook.com>
2020-04-13 00:27:50 +0200

    removed everything

--- README.md
+++
@@ -1,7 +0,0 @@
-# Arrowverse
-
-Keeping track of series progress
-
-Includes:
-- Arrowverse (Arrow, The Flash, Legends of Tomorrow)
-- Star Wars: The Clone Wars
--- public/save.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$q=$_POST["json"];
-	$p=$_POST["path"];
-	$myfile = fopen($p, "w") or die("Unable to open file!");
-	fwrite($myfile, $q);
-	fclose($myfile);
-?>
